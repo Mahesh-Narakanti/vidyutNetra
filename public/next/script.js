8ee9eeb9e21e95138d494ae70a786f291c27a4e3
Mahesh-Narakanti <119794954+Mahesh-Narakanti@users.noreply.github.com>
2024-03-28 21:32:47 +0530

    Add files via upload

---
+++ public/next/script.js
@@ -0,0 +1,11 @@
+    
+    
+    function change(){
+        window.location.href = '/last/final.html';
+    }
+    function changes(){
+        window.location.href = 'map.html';
+    }
+    function chan(){
+        window.location.href = 'predict.html';
+    }
